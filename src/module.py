5e04e1057a48d53f98f2c09b438810088ee7a32b
Giully314 <giully@protonmail.com>
2021-08-04 15:21:31 +0200

    Deleted activation function inside ArtificialNeuron

--- src/module.py
+++ src/module.py
@@ -4,19 +4,21 @@ from activations import relu
 
 
 class ArtificialNeuron:
-    def __init__(self, in_features, activation=None):
+    def __init__(self, in_features):
         
         #https://www.deeplearning.ai/ai-notes/initialization/
         self.weights = [Variable(random.uniform(-1.0, 1.0)) for _ in range(in_features)]  
         self.bias = Variable(0.0)
-        self.activation = activation if activation is not None else relu
+
+        #For now i treat the activation as something that is detached from the artificial neuron.
+        # Maybe later on, i will change this behavior. 
+        # self.activation = activation if activation is not None else relu
 
 
     def compute(self, x):
         assert(len(x) == len(self.weights))
 
         out = sum([x * v for x, v in zip(self.weights, x)], self.bias)
-        out = self.activation(out)
         return out
         
     def __call__(self, x):
