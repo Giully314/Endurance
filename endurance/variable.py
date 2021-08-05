9a8d26e96d49381fcdf971f0ce05abd57a8b1a21
Giully314 <giully@protonmail.com>
2021-08-05 22:10:05 +0200

    Moved notes inside readme

--- endurance/variable.py
+++ endurance/variable.py
@@ -7,15 +7,6 @@ from dataclasses import dataclass
 import math
 
 
-#Note: The implementation is only for educational purposes. The code could be optimized and make it more flexible.
-#I intentionally wrote the operations explicitely so i could think about what happens.
-#I just wanted to try to implement backpropagation after i studied it from 1) https://cs231n.github.io/optimization-2/ 
-#2) https://arxiv.org/abs/1502.05767 .
-
-"""
-Other resource that i looked up:    https://sidsite.com/posts/autodiff/   
-                                    https://github.com/karpathy/micrograd
-"""
 @dataclass
 class AtomicOperation(ABC):
     """
